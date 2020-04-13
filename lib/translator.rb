bd5c8aaa96c8dcec657fa5cb5a41404673cdbf26
Luis Bernal <luis_bernal8010@yahoo.com>
2020-04-13 22:35:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,6 +18,7 @@ def get_japanese_emoticon(file_path,emoticon)
     japanesen = emoticons[:japanese]
     if emoticon == english
       response = japanese
+      binding.pry
     end
   end
   if response == nil
